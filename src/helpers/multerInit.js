e8627163fc18b67a7fb4428f3db98f73e6e2927a
denis-snitko <denissnitko@gmail.com>
2023-01-14 12:43:24 +0300

    Check uploads directory

--- src/helpers/multerInit.js
+++ src/helpers/multerInit.js
@@ -1,7 +1,14 @@
+import fs from 'fs';
 import multer from 'multer';
 
 const storage = multer.diskStorage({
   destination: (_, __, cd) => {
+    const dirName = 'uploads';
+    
+    if (!fs.existsSync(dirName)) {
+      fs.mkdirSync(dirName);
+    }
+    
     cd(null, 'uploads');
   },
   filename: (_, file, cd) => {
